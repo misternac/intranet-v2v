6fc3ff4d67f4687798795d801987a97628f3b69a
Christopher N'TAKPE <christopher.ntakpe@gmail.com>
2025-05-26 15:21:38 -0400

    initialisation du projet

---
+++ administrator/logs/1.joomla_scheduler.php
@@ -0,0 +1,15 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-03-04 16:43:34 UTC
+#Software: Joomla! 5.2.2 Stable [ Uthabiti ] 26-November-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2025-03-04	16:43:34	INFO	Successfully finished task#01 in 0.00 (net 0.00) seconds.
+2025-05-22	20:51:24	INFO	Running task#02 'Session GC'.
+2025-05-22	20:51:25	INFO	Task> SessionGC end
+2025-05-22	20:51:25	INFO	Successfully finished task#02 in 0.00 (net 0.06) seconds.
+2025-05-22	20:51:34	INFO	Running task#03 'Update Notification'.
+2025-05-22	20:51:47	INFO	Task> Could not instantiate mail function.
+2025-05-22	20:51:47	INFO	Task> UpdateNotification end
+2025-05-22	20:51:47	INFO	Successfully finished task#03 in 13.08 (net 13.08) seconds.
+2025-05-26	16:31:40	INFO	Running task#01 'Rotate Logs'.
---
+++ administrator/logs/joomla_scheduler.php
@@ -0,0 +1,14 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2025-05-26 16:31:40 UTC
+#Software: Joomla! 5.2.2 Stable [ Uthabiti ] 26-November-2024 16:00 GMT
+
+#Fields: date	time	priority	message
+2025-05-26	16:31:40	INFO	Successfully finished task#01 in 0.02 (net 0.08) seconds.
+2025-05-26	16:31:58	INFO	Running task#02 'Session GC'.
+2025-05-26	16:31:58	INFO	Task> SessionGC end
+2025-05-26	16:31:58	INFO	Successfully finished task#02 in 0.00 (net 0.00) seconds.
+2025-05-26	16:32:06	INFO	Running task#03 'Update Notification'.
+2025-05-26	16:32:21	INFO	Task> Could not instantiate mail function.
+2025-05-26	16:32:21	INFO	Task> UpdateNotification end
+2025-05-26	16:32:21	INFO	Successfully finished task#03 in 14.29 (net 14.29) seconds.
